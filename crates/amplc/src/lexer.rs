53328365c2a195993ab845bda9d9d6c30e96c4ca
Marshall Bowers <elliott.codes@gmail.com>
2022-09-30 23:56:41 -0400

    Extend lexer with support for lambdas

--- crates/amplc/src/lexer.rs
+++ crates/amplc/src/lexer.rs
@@ -1,5 +1,16 @@
 use logos::Logos;
 
+/// The representation of an Ampl operator.
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum OperatorRepr {
+    /// The operator is represented using ASCII.
+    Ascii,
+
+    /// The operator is represented using Unicode.
+    Unicode,
+}
+
+/// An Ampl token.
 #[derive(Logos, Debug, PartialEq, Clone, Copy)]
 pub enum Token {
     #[token("(")]
@@ -8,14 +19,25 @@ pub enum Token {
     #[token(")")]
     RightParen,
 
+    #[token("λ", operator_repr)]
+    #[token(r"\", operator_repr)]
+    Lambda(OperatorRepr),
+
     #[regex("[a-zA-Z]+")]
     Symbol,
 
-    #[regex(r"[ \t\n\f]+", logos::skip)]
+    #[regex(r"[ \n]+", logos::skip)]
     Whitespace,
 
     #[error]
     Error,
+}
+
+fn operator_repr(lexer: &mut logos::Lexer<Token>) -> OperatorRepr {
+    match lexer.slice().is_ascii() {
+        true => OperatorRepr::Ascii,
+        false => OperatorRepr::Unicode,
+    }
 }
 
 pub struct Lexer<'a> {
@@ -38,5 +60,37 @@ impl<'a> Iterator for Lexer<'a> {
         let lexeme = self.lexer.slice();
 
         Some((token, lexeme))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn check(input: &str, token: Token) {
+        let mut lexer = Token::lexer(input);
+
+        assert_eq!(lexer.next(), Some(token));
+        assert_eq!(lexer.slice(), input);
+    }
+
+    #[test]
+    fn lex_left_paren() {
+        check("(", Token::LeftParen)
+    }
+
+    #[test]
+    fn lex_right_paren() {
+        check(")", Token::RightParen)
+    }
+
+    #[test]
+    fn lex_lambda() {
+        check("λ", Token::Lambda(OperatorRepr::Unicode))
+    }
+
+    #[test]
+    fn lex_ascii_lambda() {
+        check(r"\", Token::Lambda(OperatorRepr::Ascii))
     }
 }
