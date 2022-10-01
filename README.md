a416d78f78338e350fa799c01fb623eeb6a007e3
Marshall Bowers <elliott.codes@gmail.com>
2022-10-01 12:12:09 -0400

    Get rid of separate `amplc` crate

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ampl
+# Ampl
+
 A Mere Path Language
+
+## Development
+
+```
+cargo run -- "(. foo bar baz)" '{"foo":{"bar":{"baz":"Hello from Ampl!"}}}'
+```
--- crates/ampl/src/main.rs
+++ crates/ampl/src/main.rs
@@ -3,16 +3,18 @@ use ampl_eval::eval;
 use clap::Parser;
 
 #[derive(Parser, Debug)]
-struct Args {}
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    expr: String,
+    json: String,
+}
 
 fn main() {
     let args = Args::parse();
 
-    let json = r#"{"foo":{"bar":{"baz":"Hello from Ampl"}}}"#;
+    let compile_result = compile(&args.expr).expect("compile error");
 
-    let compile_result = compile("(. foo bar baz)").expect("compile error");
-
-    let eval_result = eval(compile_result, json).expect("evaluation error");
+    let eval_result = eval(compile_result, &args.json).expect("evaluation error");
 
     println!("{}", eval_result)
 }
